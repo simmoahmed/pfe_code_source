3b9282b48a71b13b38d45cc1fd52581c9008219c
EL MAADOUDI Mohamed <91148387+simmoahmed@users.noreply.github.com>
2024-06-13 12:33:49 +0100

    Update ExceltoAtlas.py

--- ExceltoAtlas.py
+++ ExceltoAtlas.py
@@ -1,72 +1,135 @@
+import os
 import pandas as pd
 from pyhive import hive
+from datetime import datetime
+from time import sleep
+import glob
+from unidecode import unidecode
+import warnings
+import numpy as np
+warnings.filterwarnings("ignore")
 
-def create_hive_table(table_name, schema):
-    conn = hive.Connection(host="localhost", port=10000)
-    cursor = conn.cursor()
 
-    cursor.execute(f"SHOW TABLES LIKE '{table_name}'")
-    existing_tables = cursor.fetchall()
-    if existing_tables:
-        print(f"------------Table '{table_name}' already exists in the database------------")
-        return
-
-    create_table_query = f"CREATE TABLE IF NOT EXISTS {table_name} ("
-    for column, data_type in schema.items():
-        create_table_query += f"`{column.replace(' ', '_').replace('é', 'e').replace('à', 'a').replace('ç', 'c')}` {data_type}, "
-    create_table_query = create_table_query[:-2]  
-    create_table_query += f")"
-
-    cursor.execute(create_table_query)
-    conn.commit()
-    conn.close()
-    print(f"------------Table '{table_name}' created successfully------------")
-
-def insert_data_into_hive_table(table_name, data):
-    conn = hive.Connection(host="localhost", port=10000)
-    cursor = conn.cursor()
-
-    insert_query = f"INSERT INTO TABLE {table_name} VALUES "
-    for index, row in data.iterrows():
-        values = ','.join([f"'{str(value)}'" if isinstance(value, str) else str(value) for value in row.values])
-        insert_query += f"({values}),"
-    insert_query = insert_query[:-1]
-    cursor.execute(insert_query)
-    conn.commit()
-    conn.close()
-
-def get_hive_table_schema(table_name):
-    conn = hive.Connection(host="localhost", port=10000)
-    cursor = conn.cursor()
-
-    cursor.execute(f"DESCRIBE {table_name}")
-    table_schema = {}
-    for row in cursor.fetchall():
-        column_name = row[0]
-        data_type = row[1]
-        table_schema[column_name] = data_type
-
-    conn.close()
-
-    return table_schema
-
-def excel_to_hive(excel_file):
-    xls = pd.ExcelFile(excel_file)
-    for sheet_name in xls.sheet_names:
-        df = pd.read_excel(excel_file, sheet_name=sheet_name)
-        
-        df = df.apply(lambda x: x if x.dtype != 'object' else x.apply(lambda y: str(y).replace('\'', ' ').replace('l\'', 'l ')))
-
-        column_types = df.dtypes.apply(lambda x: 'STRING' if x == 'object' else 'INT').to_dict()
-        hive_table_name = sheet_name.replace(' ', '_').replace('é', 'e').replace('à', 'a').replace('ç', 'c')
-        create_hive_table(hive_table_name, column_types)
-        insert_data_into_hive_table(hive_table_name, df)
-        hive_schema = get_hive_table_schema(hive_table_name)
-        print(f"\n--->Hive Table Schema for {hive_table_name}:")
+class ExcelOps:    
+    def create_hive_table(table_name, schema):
+        conn = hive.Connection(host="host.docker.internal", port=10000)
+        cursor = conn.cursor()
+        cursor.execute(f"SHOW TABLES LIKE '{table_name}'")
+        existing_tables = cursor.fetchall()
+        if existing_tables:
+            print(f"------------Table '{table_name}' already exists in the database------------")
+            return
+        create_table_query = f"CREATE TABLE IF NOT EXISTS {table_name} ("
+        for column, data_type in schema.items():
+            create_table_query += f"`{column.replace(' ', '_').replace('é', 'e').replace('à', 'a').replace('ç', 'c')}` {data_type}, "
+        create_table_query = create_table_query[:-2]  
+        create_table_query += f")"        
+        cursor.execute(create_table_query)
+        conn.commit()
+        conn.close()
+        print(f"------------Table '{table_name}' created successfully------------")
+        hive_schema = ExcelOps.get_hive_table_schema(table_name)
+        print(f"\n--->Hive Table Schema for {table_name}:")
         for column, data_type in hive_schema.items():
             print(f"{column}: {data_type}")
         print("-----------------------------------")
-    print("Tables are created successfully in the Hive database.")
 
-excel_file = 'Data_Cata_1.xlsx'
-excel_to_hive(excel_file)
+    def clean_value(value):
+        if pd.isnull(value):
+            return 'null'
+        elif isinstance(value, str):
+            cleaned_value = unidecode(value)
+            cleaned_value = cleaned_value.replace("'", "")
+            return f"'{cleaned_value}'"
+        elif isinstance(value, datetime):
+            return f"'{value}'"
+        elif isinstance(value, (int, float)):
+            return str(value)
+        else:
+            return "'"+str(value)+"'"
+
+    def insert_data_into_hive_table(table_name, data):
+        conn = hive.Connection(host="host.docker.internal", port=10000)
+        cursor = conn.cursor()
+        cursor.execute(f"SELECT * FROM {table_name}")
+        existing_data = cursor.fetchall()
+        insert_query = f"INSERT INTO TABLE {table_name} VALUES "
+        for index, row in data.iterrows():
+            values = ', '.join([ExcelOps.clean_value(value) for value in row.values])
+            if "("+values+")" not in str(existing_data):
+                insert_query += f"({values}),"
+        if insert_query != f"INSERT INTO TABLE {table_name} VALUES ":
+            insert_query = insert_query[:-1]
+            cursor.execute(insert_query)
+            conn.commit()
+        conn.close()
+
+    def get_hive_table_schema(table_name):
+        conn = hive.Connection(host="host.docker.internal", port=10000)
+        cursor = conn.cursor()
+
+        cursor.execute(f"DESCRIBE {table_name}")
+        table_schema = {}
+        for row in cursor.fetchall():
+            column_name = row[0]
+            data_type = row[1]
+            table_schema[column_name] = data_type
+
+        conn.close()
+
+        return table_schema
+
+    def infer_column_types(df):
+        for col in df.columns:
+            if df[col].dtype in ['int64','float64']:
+                continue    
+            try:
+                df[col] = pd.to_datetime(df[col], infer_datetime_format=True)
+            except:
+                continue
+        column_types = {}
+        for column in df.columns:
+            for value in df[column]:
+                if isinstance(value, int):
+                    column_types[column] = 'BIGINT'
+                    break
+                elif isinstance(value, float):
+                    column_types[column] = 'DOUBLE'
+                    break
+                elif isinstance(value, datetime):
+                    column_types[column] = 'DATE'
+                    df[column] = pd.to_datetime(df[column]).dt.date
+                    break
+                elif pd.api.types.is_string_dtype(df[column]):
+                    column_types[column] = 'STRING'
+                    break
+        return column_types
+
+
+def excel_to_hive(excel_file):
+    excel_data = pd.ExcelFile(excel_file, engine='openpyxl')
+    for sheet_name in excel_data.sheet_names:
+        print(f"Processing sheet: {sheet_name}")
+        df = pd.read_excel(excel_file, sheet_name=sheet_name, engine='openpyxl')
+        column_types = ExcelOps.infer_column_types(df)
+        table_name = sheet_name.replace('.', '')
+        
+        ExcelOps.create_hive_table(table_name, column_types)
+        ExcelOps.insert_data_into_hive_table(table_name, df)
+        hive_schema = ExcelOps.get_hive_table_schema(table_name)
+        print(f"\n--->Hive Table Schema for {table_name}:")
+        for column, data_type in hive_schema.items():
+            print(f"{column}: {data_type}")
+        print("-----------------------------------")
+        print(f"Table '{table_name}' is created successfully in the Hive database.")
+
+
+def process_new_excel_files(**kwargs):
+    path_to_watch = '/opt/airflow/dags/data/'
+    excel_files = glob.glob(os.path.join(path_to_watch, '*.xlsx'))
+    
+    for excel_file in excel_files:
+        print(f"Processing file: {excel_file}")
+        excel_to_hive(excel_file)
+
+process_new_excel_files()
